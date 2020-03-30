5256a4e4199eaed7f08d1e624a2162b158aef882
Leo Crestel <crestel.leopold@gmail.com>
2020-03-30 18:54:52 +0200

    test udp up

--- Max_server/client_test.py
+++ Max_server/client_test.py
@@ -1,10 +1,10 @@
 import socket
 
-UDP_IP = "63.33.36.17"
-UDP_PORT = 5002
+UDP_IP = "212.11.40.145"
+UDP_PORT = 5001
 MESSAGE = "Yo"
 
 print(f'Sending {MESSAGE} at {UDP_IP} on port {UDP_PORT}')
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(str.encode(MESSAGE), (UDP_IP, UDP_PORT))
--- Max_server/server_test.py
+++ Max_server/server_test.py
@@ -1,7 +1,7 @@
 import socket
 
 UDP_IP = "0.0.0.0"
-UDP_PORT = 5002
+UDP_PORT = 5001
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
 sock.bind((UDP_IP, UDP_PORT))
