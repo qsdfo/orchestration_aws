2d7d5a9dd86b8efcd47a16cdd1b2f1050047edc4
Leo Crestel <crestel.leopold@gmail.com>
2020-03-30 18:57:44 +0200

    test udp up

--- Max_server/client_test.py
+++ Max_server/client_test.py
@@ -5,6 +5,8 @@ UDP_PORT = 5001
 MESSAGE = "Yo"
 
 print(f'Sending {MESSAGE} at {UDP_IP} on port {UDP_PORT}')
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
+sock = socket.socket(socket.AF_INET,
+                     socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.sendto(str.encode(MESSAGE), (UDP_IP, UDP_PORT))
